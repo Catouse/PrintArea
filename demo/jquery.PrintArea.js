7add02eced41d737b9f9ab8514d01e89d1ef0e0b
Catouse <catouse@gmail.com>
2013-05-29 11:46:38 +0800

    选项增加了对style的支持，支持打印时加入额外的样式表或者在body插入新的类
    同时也调整了原文件中缺少分号的格式错误

--- demo/jquery.PrintArea.js
+++ demo/jquery.PrintArea.js
@@ -15,6 +15,7 @@
  *  {OPTIONS} | [type]    | (default), values      | Explanation
  *  --------- | --------- | ---------------------- | -----------
  *  @mode     | [string]  | ("iframe"),"popup"     | printable window is either iframe or browser popup
+ *  @style    | [string]  | ('')                   | 加载打印时的额外样式，如果为空，则仅加载原页面样式，如果为css文件，则加载到打印页面的head部分，如果为类名，则在打印页面body标签增加该类
  *  @popHt    | [number]  | (500)                  | popup window height
  *  @popWd    | [number]  | (400)                  | popup window width
  *  @popX     | [number]  | (500)                  | popup window screen X position
@@ -23,135 +24,134 @@
  *  @popClose | [boolean] | (false),true           | popup window close after printing
  *  @strict   | [boolean] | (undefined),true,false | strict or loose(Transitional) html 4.01 document standard or undefined to not include at all (only for popup option)
  */
-(function($) {
+(function ($) {
     var counter = 0;
-    var modes = { iframe : "iframe", popup : "popup" };
-    var defaults = { mode     : modes.iframe,
-                     popHt    : 500,
-                     popWd    : 400,
-                     popX     : 200,
-                     popY     : 200,
-                     popTitle : '',
-                     popClose : false };
+    var modes = { iframe: "iframe", popup: "popup" };
+    var defaults = {
+        mode: modes.iframe,
+        style: '',
+        popHt: 500,
+        popWd: 400,
+        popX: 200,
+        popY: 200,
+        popTitle: '',
+        popClose: false
+    };
 
     var settings = {};//global settings
 
-    $.fn.printArea = function( options )
-        {
-            $.extend( settings, defaults, options );
+    $.fn.printArea = function (options) {
+        $.extend(settings, defaults, options);
 
-            counter++;
-            var idPrefix = "printArea_";
-            $( "[id^=" + idPrefix + "]" ).remove();
-            var ele = getFormData( $(this) );
+        counter++;
+        var idPrefix = "printArea_";
+        $("[id^=" + idPrefix + "]").remove();
+        var ele = getFormData($(this));
 
-            settings.id = idPrefix + counter;
+        settings.id = idPrefix + counter;
+        settings.hasStyleFile = settings.style.toLowerCase().indexOf(".css", settings.style.length - 4) != -1;
+        settings.hasStyleClass = (!settings.hasStyleFile) && settings.style != undefined && settings.style != "";
 
-            var writeDoc;
-            var printWindow;
+        var writeDoc;
+        var printWindow;
 
-            switch ( settings.mode )
-            {
-                case modes.iframe :
-                    var f = new Iframe();
-                    writeDoc = f.doc;
-                    printWindow = f.contentWindow || f;
-                    break;
-                case modes.popup :
-                    printWindow = new Popup();
-                    writeDoc = printWindow.doc;
-            }
-
-            writeDoc.open();
-            writeDoc.write( docType() + "<html>" + getHead() + getBody(ele) + "</html>" );
-            writeDoc.close();
-
-            printWindow.focus();
-            printWindow.print();
-
-            if ( settings.mode == modes.popup && settings.popClose )
-                printWindow.close();
+        switch (settings.mode) {
+            case modes.iframe:
+                var f = new Iframe();
+                writeDoc = f.doc;
+                printWindow = f.contentWindow || f;
+                break;
+            case modes.popup:
+                printWindow = new Popup();
+                writeDoc = printWindow.doc;
         }
 
-    function docType()
-    {
-        if ( settings.mode == modes.iframe || !settings.strict ) return "";
+        writeDoc.open();
+        writeDoc.write(docType() + "<html>" + getHead() + getBody(ele) + "</html>");
+        writeDoc.close();
+
+        printWindow.focus();
+        printWindow.print();
+
+        if (settings.mode == modes.popup && settings.popClose)
+            printWindow.close();
+    };
+
+    function docType() {
+        if (settings.mode == modes.iframe || !settings.strict) return "";
 
         var standard = settings.strict == false ? " Trasitional" : "";
         var dtd = settings.strict == false ? "loose" : "strict";
 
-        return '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01' + standard + '//EN" "http://www.w3.org/TR/html4/' + dtd +  '.dtd">';
+        return '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01' + standard + '//EN" "http://www.w3.org/TR/html4/' + dtd + '.dtd">';
     }
 
-    function getHead()
-    {
+    function getHead() {
         var head = "<head><title>" + settings.popTitle + "</title>";
         $(document).find("link")
-            .filter(function(){ return $(this).attr("rel").toLowerCase() == "stylesheet"; })
-            .filter(function(){
-                    var media = $(this).attr("media");
-                    return (media == undefined || media.toLowerCase() == "" || media.toLowerCase() == "print")
-                })
-            .each(function(){
-                    head += '<link type="text/css" rel="stylesheet" href="' + $(this).attr("href") + '" >';
-                });
+            .filter(function () { return $(this).attr("rel").toLowerCase() == "stylesheet"; })
+            .filter(function () {
+                var media = $(this).attr("media");
+                return (media == undefined || media.toLowerCase() == "" || media.toLowerCase() == "print");
+            })
+            .each(function () {
+                head += '<link type="text/css" rel="stylesheet" href="' + $(this).attr("href") + '" >';
+            });
 
+        if (settings.hasStyleFile) {
+            head += '<link type="text/css" rel="stylesheet" href="' +settings.style + '" >';
+        }
         return head += "</head>";
     }
 
-    function getBody( printElement )
-    {
-        return '<body><div class="' + $(printElement).attr("class") + '">' + $(printElement).html() + '</div></body>';
+    function getBody(printElement) {
+        return '<body class="'+(settings.hasStyleClass?settings.style:'')+'"><div class="' + $(printElement).attr("class") + '">' + $(printElement).html() + '</div></body>';
     }
 
-    function getFormData( ele )
-    {
+    function getFormData(ele) {
         var copy = ele.clone();
         var copiedInputs = $("input,select,textarea", copy);
-        $("input,select,textarea", ele).each(function( i ){
+        $("input,select,textarea", ele).each(function (i) {
             var type = $(this).attr("type");
             if (type == undefined) type = $(this).is("select") ? "select" : $(this).is("textarea") ? "textarea" : "";
-            var copiedInput = copiedInputs.eq( i );
+            var copiedInput = copiedInputs.eq(i);
 
-            if ( type == "radio" || type == "checkbox" ) copiedInput.attr( "checked", $(this).is(":checked") );
-            else if ( type == "text" ) copiedInput.attr( "value", $(this).val() );
-            else if ( type == "select" )
-                $(this).find( "option" ).each( function( i ) {
-                    if ( $(this).is(":selected") ) $("option", copiedInput).eq( i ).attr( "selected", true );
+            if (type == "radio" || type == "checkbox") copiedInput.attr("checked", $(this).is(":checked"));
+            else if (type == "text") copiedInput.attr("value", $(this).val());
+            else if (type == "select")
+                $(this).find("option").each(function (i) {
+                    if ($(this).is(":selected")) $("option", copiedInput).eq(i).attr("selected", true);
                 });
-            else if ( type == "textarea" ) copiedInput.text( $(this).val() );
+            else if (type == "textarea") copiedInput.text($(this).val());
         });
         return copy;
     }
 
-    function Iframe()
-    {
+    function Iframe() {
         var frameId = settings.id;
         var iframeStyle = 'border:0;position:absolute;width:0px;height:0px;left:0px;top:0px;';
         var iframe;
 
-        try
-        {
+        try {
             iframe = document.createElement('iframe');
             document.body.appendChild(iframe);
             $(iframe).attr({ style: iframeStyle, id: frameId, src: "" });
             iframe.doc = null;
-            iframe.doc = iframe.contentDocument ? iframe.contentDocument : ( iframe.contentWindow ? iframe.contentWindow.document : iframe.document);
+            iframe.doc = iframe.contentDocument ? iframe.contentDocument : (iframe.contentWindow ? iframe.contentWindow.document : iframe.document);
         }
-        catch( e ) { throw e + ". iframes may not be supported in this browser."; }
+        catch (e) { throw e + ". iframes may not be supported in this browser."; }
 
-        if ( iframe.doc == null ) throw "Cannot find document.";
+        if (iframe.doc == null) throw "Cannot find document.";
 
         return iframe;
     }
 
-    function Popup()
-    {
+    function Popup() {
         var windowAttr = "location=yes,statusbar=no,directories=no,menubar=no,titlebar=no,toolbar=no,dependent=no";
         windowAttr += ",width=" + settings.popWd + ",height=" + settings.popHt;
         windowAttr += ",resizable=yes,screenX=" + settings.popX + ",screenY=" + settings.popY + ",personalbar=no,scrollbars=no";
 
-        var newWin = window.open( "", "_blank",  windowAttr );
+        var newWin = window.open("", "_blank", windowAttr);
 
         newWin.doc = newWin.document;
 
